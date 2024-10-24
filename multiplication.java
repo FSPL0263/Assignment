73a321a4f34c3da70c3ae45dc208ac902ba71755
suyash mahajan <suyashmahajan15@gmail.com>
2024-10-24 12:57:52 +0530

    add new file

---
+++ multiplication.java
@@ -0,0 +1,14 @@
+public class multiplication {
+    public static void main(String[] args) {
+
+            int num1 = 5;
+            int num2 = 7;
+
+            int result = num1 * num2;
+
+
+            System.out.println("The result of multiplication is: " + result);
+        }
+}
+
+
